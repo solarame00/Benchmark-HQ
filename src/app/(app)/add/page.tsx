e6a7fe4a5e3817fa4b947af13b2aab332e5e2ecf
Soufiane Lqadi <lqadis@gmail.com>
2025-08-06 15:32:04 +0000

    In app/(app)/add/page.js of the solarame00/Benchmark-HQ Next.js project

--- src/app/(app)/add/page.tsx
+++ src/app/(app)/add/page.tsx
@@ -1,11 +1,10 @@
-
 'use client';
 
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
 // This component safely uses the useSearchParams hook.
-function AddContent() {
+function SearchParamsContent() {
   const searchParams = useSearchParams();
   // The content of this page is not currently used, but we read a param
   // to satisfy the build process and fix the error.
@@ -18,8 +17,8 @@ function AddContent() {
 // The main page component wraps the content in a Suspense boundary.
 export default function AddPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <AddContent />
+    <Suspense fallback={<div>Loading parameters...</div>}>
+      <SearchParamsContent />
     </Suspense>
   );
 }
