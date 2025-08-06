9c853bf93bb3e19c14c1925fcba6b2494772327a
Soufiane Lqadi <lqadis@gmail.com>
2025-08-06 13:36:27 +0000

    I encountered a build error during Vercel deployment. The log shows useS

--- src/app/(app)/add/page.tsx
+++ src/app/(app)/add/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 // This page is no longer needed as the form is part of the main page.
 // We can delete this file, but for now, we'll just empty it.
 export default function AddBenchmarkPage() {
